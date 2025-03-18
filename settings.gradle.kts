965a4d72341c5dbb9edb736a4cdc8e03ce826a23
John Blum <john.blum@earnin.com>
2025-03-18 11:23:30 -0700

    Declare pluginManagement in settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("io.spring.dependency-management") version "1.1.7"
+    }
+}
+
 rootProject.name = "spring-boot-jar-shading"
 
 include("services:serviceA:client")
 include("services:serviceB:client")
+include("services:serviceC")
 include("app")
